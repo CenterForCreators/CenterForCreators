40fe757ec13181d001292925c24d01064cc62c8e
Joseph Jung <joseph@hologram.io>
2022-03-25 17:16:25 -0500

    initial commit.

---
+++ metabrowser/app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+function hello(world) {
+    if (world === void 0) { world = "WHAT"; }
+    return "Hello ".concat(world, "! ");
+}
+exports.hello = hello;
+console.log(hello());
+//# sourceMappingURL=app.js.map
